25ad352c0c640b477eb710862b8358597c78f229
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-01-28 13:26:04 -0500

    (feat): Changed delay for carousel update

--- src/scripts/switchCarousel.js
+++ src/scripts/switchCarousel.js
@@ -1,22 +1,22 @@
 export function navigateCarouselOnLoad() {
-    function getURLParameter(name) {
-        return new URLSearchParams(window.location.search).get(name);
+  function getURLParameter(name) {
+    return new URLSearchParams(window.location.search).get(name);
+  }
+
+  const projectIndex = getURLParameter("projectIndex");
+  if (projectIndex !== null && !isNaN(projectIndex)) {
+    // Scroll to the "Other Projects" section
+    const otherProjectsSection = document.querySelector("#other-projects");
+    if (otherProjectsSection) {
+      otherProjectsSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     }
 
-    const projectIndex = getURLParameter("projectIndex");
-    if (projectIndex !== null && !isNaN(projectIndex)) {
-        // Scroll to the "Other Projects" section
-        const otherProjectsSection = document.querySelector("#other-projects");
-        if (otherProjectsSection) {
-            otherProjectsSection.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
-            });
-        }
-
-        // Then navigate to the specific slide after a short delay to allow for scrolling
-        setTimeout(() => {
-            $("#projectsCarousel").carousel(parseInt(projectIndex));
-        }, 500); // Adjust delay as needed
-    }
+    // Then navigate to the specific slide after a short delay to allow for scrolling
+    setTimeout(() => {
+      $("#projectsCarousel").carousel(parseInt(projectIndex));
+    }, 1000); // Adjust delay as needed
+  }
 }
