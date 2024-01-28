1678895a5b57bc9dfd50a69d4e32092c79efa241
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-01-28 01:18:49 -0500

    (fix): Discared scrolling feature

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import "bootstrap";
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-import {
-  applyPaddingOnLoad,
-  navigateCarouselOnLoad,
-} from "./scripts/customNavigation";
 
-document.addEventListener("DOMContentLoaded", () => {
-  initScrollReveal(targetElements, defaultProps);
-  initTiltEffect();
-  applyPaddingOnLoad();
-  navigateCarouselOnLoad();
-});
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
