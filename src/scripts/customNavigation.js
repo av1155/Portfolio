1db90e08846cfd43d280c61dbd4bc628565ab929
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-01-27 23:31:10 -0500

    (feat): Changed scroll to carousel method

--- src/scripts/customNavigation.js
+++ src/scripts/customNavigation.js
@@ -7,16 +7,6 @@ export function scrollToElementWithPadding(elementId, padding = "50px") {
   }
 }
 
-// Function to scroll to an element with an offset
-export function scrollToElementWithOffset(elementId, offset = 100) {
-  const element = document.getElementById(elementId);
-  if (element) {
-    const elementPosition =
-      element.getBoundingClientRect().top + window.scrollY;
-    window.scrollTo({ top: elementPosition - offset, behavior: "smooth" });
-  }
-}
-
 // Function to apply padding when the page loads for all anchor links
 export function applyPaddingOnLoad() {
   const anchorLinks = [
@@ -52,12 +42,10 @@ export function navigateCarouselOnLoad() {
 
   const projectIndex = getURLParameter("projectIndex");
   if (projectIndex !== null && !isNaN(projectIndex)) {
-    // Scroll to the carousel first with offset
-    scrollToElementWithOffset("projectsCarousel", 100); // Custom offset for carousel
+    scrollToElementWithPadding("carouselAnchor", "100px");
 
-    // Then navigate to the specific slide after a short delay
     setTimeout(() => {
       $("#projectsCarousel").carousel(parseInt(projectIndex));
-    }, 500); // Adjust delay as needed
+    }, 500);
   }
 }
