286316074bc50c3cd63f147e513c877ad2bc576a
Andrea Venti Fuentes <117413846+av1155@users.noreply.github.com>
2024-01-28 23:23:19 -0500

    (feat): Updated Mastermind project; updated resume

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 import { adjustScrollForAbout } from "./scripts/adjustScroll.js";
-import "./scripts/switchCarousel"; // Added this line
+import "./scripts/switchCarousel";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
